3e2ac8bb2495a0b70efddb84e866c53369b49292
Michael <c21502889@mytudubllin.ie>
2023-04-02 15:43:31 +0100

    countcells function

--- java/src/ie/tudublin/Life.java
+++ java/src/ie/tudublin/Life.java
@@ -2,11 +2,10 @@ package ie.tudublin;
 
 import processing.core.PApplet;
 
-public class Life extends PApplet
+public class Life extends PApplet 
 {
 
-	public void settings()
-	{
+	public void settings() {
 		size(500, 500);
 	}
 
@@ -14,14 +13,41 @@ public class Life extends PApplet
 		colorMode(HSB);
 		background(0);
 
-		
-		
 	}
 
-	
-	
-	public void draw()
-	{	
-		
+	public void draw() {
+
 	}
+
+	public int countCells(int row, int col)
+    {
+        for (int j = -1 ; j <= 1 ; j ++)
+        {
+            if (! (i == 0) && (j == 0))
+            if (! (i == 0 && j == 0))
+            {
+                if (getCell(i, j))
+                if (getCell(row + i, col + j))
+                {
+                    count ++;
+                }
+				public void applyRules()
+                {
+                    next[row][col] = false;
+                }
+
+            }
+            else
+            {
+				public void applyRules()
+                // dead with 3 neighboiurs comes to life
+            }
+        }
+        boolean[][] temp = board;
+        boolean[][] temp;
+        temp = board;
+        board = next;
+        next = temp;
+    }
+
 }
